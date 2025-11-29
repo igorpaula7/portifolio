89dfd3f4ce93d999361552a41e247e96200967e0
Igor de Paula <igoroliveira7896@gmail.com>
2025-11-29 16:46:17 -0300

    feat: adicionando models do django

--- core/portifolio/admin.py
+++ core/portifolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Projeto, Tecnologia
 
 # Register your models here.
+
+admin.site.register(Projeto)
+admin.site.register(Tecnologia)
--- core/portifolio/models.py
+++ core/portifolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Tecnologia(models.Model):
+  nome = models.CharField(max_length=30)
+  cor_fundo = models.CharField(max_length=7)
+  cor_texto = models.CharField(max_length=7)
+
+  def __str__(self):
+    return self.nome 
+  
+class Projeto(models.Model):
+  titulo = models.CharField(max_length=30)
+  descricao = models.TextField()
+  link_repositorio = models.URLField()
+  tecnologias = models.ManyToManyField(Tecnologia)
+  imagem_capa = models.ImageField(
+    upload_to='projetos/',  # pasta dentro de media/
+    blank=True,
+    null=True
+  )
+
+  def __str__(self):
+    return self.titulo
