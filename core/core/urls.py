0bf1b80944392fbf7ef9756ddea8b4131e959330
Igor de Paula <igoroliveira7896@gmail.com>
2025-12-22 02:12:56 +0000

    fix(media): adiciona serving de arquivos MEDIA no urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -8,3 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ListaDeProjetos.as_view(), name='lista_projetos')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
