744c313be62a09449d0e32b551842a55862c716a
Igor de Paula <igoroliveira7896@gmail.com>
2025-12-24 14:14:51 -0300

    refactor: aplica configurações de segurança no ambiente de produção

--- core/core/settings.py
+++ core/core/settings.py
@@ -67,6 +67,14 @@ else: # production, considerando PythonAnyWhere
         }
     }
 
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+    SECURE_HSTS_SECONDS = 31536000  # 1 ano
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
