9a017362fff7666023a86214ad76ffd35b841174
Igor de Paula <igoroliveira7896@gmail.com>
2025-12-24 14:01:20 -0300

    feat: adição de listagem de todos os projetos, e estilização

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from portifolio.views import ListaDeProjetos
+from portifolio.views import ListaDeProjetos, ListaDeTodosProjetos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ListaDeProjetos.as_view(), name='lista_projetos')
+    path('', ListaDeProjetos.as_view(), name='lista_projetos'),
+    path('projetos/', ListaDeTodosProjetos.as_view(), name='lista_todos_projetos')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- core/portifolio/views.py
+++ core/portifolio/views.py
@@ -10,3 +10,9 @@ class ListaDeProjetos(ListView):
 
   def get_queryset(self):
     return Projeto.objects.filter(fixado=True)[:3]
+  
+
+class ListaDeTodosProjetos(ListView):
+  model = Projeto
+  template_name = 'projeto_list_all.html'
+  context_object_name = 'projetos'
