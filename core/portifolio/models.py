d25866a427f54ff1451b82d514e5cd83efb9bf97
Igor de Paula <igoroliveira7896@gmail.com>
2026-01-15 18:56:06 -0300

    refactor(models): removendo campos de cor do model Tecnologia

--- core/portifolio/models.py
+++ core/portifolio/models.py
@@ -1,27 +1,27 @@
 from django.db import models
 
+
 class Tecnologia(models.Model):
-  nome = models.CharField(max_length=30)
-  cor_fundo = models.CharField(max_length=7)
-  cor_texto = models.CharField(max_length=7)
+    nome = models.CharField(max_length=30)
 
-  def __str__(self):
-    return self.nome 
-  
+    def __str__(self):
+        return self.nome
+
+
 class Projeto(models.Model):
-  titulo = models.CharField(max_length=30)
-  descricao = models.TextField()
-  link_repositorio = models.URLField()
-  tecnologias = models.ManyToManyField(Tecnologia)
-  imagem_capa = models.ImageField(
-    upload_to='projetos/',  # pasta dentro de media/
-    blank=True,
-    null=True
-  )
-  fixado = models.BooleanField(default=False)
+    titulo = models.CharField(max_length=30)
+    descricao = models.TextField()
+    link_repositorio = models.URLField()
+    tecnologias = models.ManyToManyField(Tecnologia)
+    imagem_capa = models.ImageField(
+        upload_to="projetos/",  # pasta dentro de media/
+        blank=True,
+        null=True,
+    )
+    fixado = models.BooleanField(default=False)
 
-  def __str__(self):
-    return self.titulo
-  
-  class Meta:
-    ordering = ['-fixado', '-id']
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        ordering = ["-fixado", "-id"]
